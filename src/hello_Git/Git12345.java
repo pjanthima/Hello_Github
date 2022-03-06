cea2d9c33f043a88b40f5d016a5aafa3ad0ce467
Phanuwat Janthima <phanuwatjanthima@Phanuwats-MBP.fios-router.home>
2022-03-06 00:22:42 -0500

    Initial commit

---
+++ src/hello_Git/Git12345.java
@@ -0,0 +1,11 @@
+package hello_Git;
+
+public class Git12345 {
+
+	public static void main(String[] args) {
+		System.out.println("git12345");
+		System.out.println("gitigigitgitgtigtitgi");
+		
+	}
+
+}
