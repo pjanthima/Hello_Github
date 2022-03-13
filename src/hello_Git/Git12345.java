22de9c788681e616c812456791b0cd1d744c516a
toeinaka <95002954+toeinaka@users.noreply.github.com>
2022-03-13 15:45:27 -0400

    Updated

--- src/hello_Git/Git12345.java
+++ src/hello_Git/Git12345.java
@@ -3,7 +3,7 @@ package hello_Git;
 public class Git12345 {
 
 	public static void main(String[] args) {
-		System.out.println("hello Risa");
+		
 		System.out.println("Hello Jane");
 		System.out.println("Hello Nucharee");
 		
