568a6772d66eda8c54f33b3cec9edd4912070b47
Phanuwat Janthima <phanuwatjanthima@Phanuwats-MBP.fios-router.home>
2022-03-13 18:01:29 -0400

    Update 6pm

--- src/hello_Git/Git12345.java
+++ src/hello_Git/Git12345.java
@@ -3,21 +3,9 @@ package hello_Git;
 public class Git12345 {
 
 	public static void main(String[] args) {
-		System.out.println("hello Risa");
-		System.out.println("Hello Jane");
-		System.out.println("Hello Nucharee");
+	System.out.println("Hello Risa 6pm");
 		
-		System.out.println("Tom1234");
-		System.out.println("Henry   ");
-		System.out.println("Shafkat");
 		
-		System.out.println("hello Risa");
-		System.out.println("Hello Jane");
-		System.out.println("Hello Nucharee");
-		
-		System.out.println("Tom1234");
-		System.out.println("Henry   ");
-		System.out.println("Shafkat");
 	}
 
 }
