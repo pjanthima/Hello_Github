75b8ab4d0b3affccaf880a72417c2c411ab4cb61
toeinaka <95002954+toeinaka@users.noreply.github.com>
2022-03-13 20:06:03 -0400

    We are so beautiful and smart and richgit statusgit status

--- src/hello_Git/Git12345.java
+++ src/hello_Git/Git12345.java
@@ -5,7 +5,7 @@ public class Git12345 {
 	public static void main(String[] args) {
 	System.out.println("Good Job Everyone!!!");
 	
-	System.out.println("9.00");
+	System.out.println("my back!!! my legs!!! 8.02pm");
 	
 	
 		
