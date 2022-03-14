298dfa89c2f4a57b55abf1bf2c20a7618a4f8fbb
Phanuwat Janthima <phanuwatjanthima@Phanuwats-MBP.fios-router.home>
2022-03-13 20:17:32 -0400

    Update
    update -Tom

--- src/hello_Git/Git12345.java
+++ src/hello_Git/Git12345.java
@@ -3,9 +3,9 @@ package hello_Git;
 public class Git12345 {
 
 	public static void main(String[] args) {
-	System.out.println("Good Job Everyone!!!");
-	
 	System.out.println("9.00");
+	
+	System.out.println("We will Practice again - Tom");
 	
 	
 		
